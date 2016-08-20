9fd61b30db6cdeaad86e7e67235a0726cf7a5876
Suleman Imdad <you@example.com>
2016-08-19 18:47:40 -0700

    Initial Commit

---
+++ counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  counter
+//
+//  Created by Suleman Imdad on 8/19/16.
+//  Copyright © 2016 Suleman Imdad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
